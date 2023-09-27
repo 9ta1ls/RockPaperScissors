27b05d463e3fc0e256ec8b906b61f6e9df7bae4b
9ta1ls <rpeleh19@gmail.com>
2023-09-27 12:40:50 +0300

    Prettier version

--- gameLogic.js
+++ gameLogic.js
@@ -12,32 +12,31 @@ possibleChoises.forEach((button) => {
 })
 
 function computerChoiseShow(){
-    let randomNum = Math.floor(Math.random() * 3);
-    if (randomNum === 0)
-        computerChoise.innerHTML = "Paper";
-    if (randomNum === 1)
-        computerChoise.innerHTML = "Rock";
-    if (randomNum === 2)
-        computerChoise.innerHTML = "Scissors";
+    let choisesList = ["Paper" , "Rock", "Scissors"];
+    let randomChoise = Math.floor(Math.random()*choisesList.length);
+    computerChoise.innerHTML = choisesList[randomChoise];
 }
 
 function finalResult(){
     const computerChoiseText = computerChoise.textContent;
     const userChoiseText = userChoise.textContent;
-    if (computerChoiseText === userChoiseText)
+    let resultTemp;
+    if (computerChoiseText === userChoiseText){
         result.innerHTML = "Draw!";
-    else if(computerChoiseText === "Paper" && userChoiseText === "Rock")
-        result.innerHTML = "You lose!";
-    else if(computerChoiseText === "Paper" && userChoiseText === "Scissors")
-        result.innerHTML = "You win!";
-
-    else if(computerChoiseText === "Rock" && userChoiseText === "Paper")
-        result.innerHTML = "You win!";
-    else if(computerChoiseText === "Rock" && userChoiseText === "Scissors")
-        result.innerHTML = "You lose!";
-    
-    else if(computerChoiseText === "Scissors" && userChoiseText === "Rock")
-        result.innerHTML = "You win!";
-    else if(computerChoiseText === "Scissors" && userChoiseText === "Paper")
-        result.innerHTML = "You lose!";
+        return;
+    }
+    switch(computerChoiseText){
+        case "Paper": {
+            result.innerHTML = userChoiseText === "Scissors"?"You won!":"You lose!";
+            break;
+        }
+        case "Rock": {
+            result.innerHTML = userChoiseText === "Paper"?"You won!":"You lose!";
+            break;
+        }
+        case "Scissors": {
+            result.innerHTML = userChoiseText === "Rock"?"You won!":"You lose!";
+            break;
+        }
+    }
 }
